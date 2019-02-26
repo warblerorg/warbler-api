87939e9c0eaf3cd6a4d9abf747ebe2303e62c73b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-26 19:45:14 +0000

    Deploy warblerorg/warbler-api to github.com/warblerorg/warbler-api.git:gh-pages

--- index.js
+++ index.js
@@ -127,10 +127,7 @@ SwaggerUIBundle({
     url: './swagger.yaml',
     dom_id: '#swagger-ui',
     deepLinking: true,
-    presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIStandalonePreset,
-    ],
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
     plugins: [SwaggerUIBundle.plugins.DownloadUrl],
     layout: 'StandaloneLayout',
 });
